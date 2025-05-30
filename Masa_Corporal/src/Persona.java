c94487a955b5d48dc50c00f02800ace872cd7a71
Jorge-Moranchel <20243ds059@utez.edu.mx>
2025-05-30 09:11:04 -0600

    Ya termine el persona con los getters and setters pero ya quedo ahora falta el main cualquier cosa cambio todo

---
+++ Masa_Corporal/src/Persona.java
@@ -0,0 +1,41 @@
+public class Persona {
+    
+    private String nombre;
+    private double altura;
+    private double peso;
+
+    public double calcularIMC() {
+        return peso / (altura * altura);
+    }
+    public String clasificarIMC() {
+        double imc = calcularIMC();
+
+        if (imc < 18.5) {
+            return "Bajo peso";
+        } else if (imc < 24.9) {
+            return "Normal";
+        } else if (imc < 29.9) {
+            return "Sobrepeso";
+        } else {
+            return "Obesidad";
+        }
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public double getAltura() {
+        return altura;
+    }
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }  
+}
